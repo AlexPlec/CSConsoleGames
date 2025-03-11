8641e1b6a627423dc84cef846d56e3c846454b44
AlexPlec <acchtc724@gmail.com>
2025-03-11 11:38:43 +0200

    update

---
+++ LocalDB/Migrations/20250311093526_MigrationName.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "blackjack",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GameResult = table.Column<string>(type: "TEXT", nullable: false),
+                    PlayerValue = table.Column<int>(type: "INTEGER", nullable: false),
+                    DealerValue = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_blackjack", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "guessTheNumber",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Attempts = table.Column<int>(type: "INTEGER", nullable: false),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_guessTheNumber", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "blackjack");
+
+            migrationBuilder.DropTable(
+                name: "guessTheNumber");
+        }
+    }
+}
